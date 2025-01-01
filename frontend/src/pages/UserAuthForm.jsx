a785b71d8219d8d48d929c41411d6b7a203a7b62
Mohd Zubair Ahmed <jubaira646@gmail.com>
2025-01-02 01:18:18 +0530

    impl. SignIn/Up logic & connection to Backend

--- frontend/src/pages/UserAuthForm.jsx
+++ frontend/src/pages/UserAuthForm.jsx
@@ -4,31 +4,72 @@ import googleIcon from "../imgs/google.png";
 import { Link } from "react-router-dom";
 import AnimationWrapper from "../common/AnimationWrapper";
 import { useRef } from "react";
+import {Toaster, toast} from 'react-hot-toast';
+import axios from 'axios';
 
 const UserAuthForm = ({ type }) => {
 
-  const authForm = useRef(null);
 
-  const handleSubmit =(event)=>{
+  // const authForm = useRef(null);
+  // use useRef react hook we can reference the form here and can use the authForm in any function to use the form data
+  const userAuthThroughServer = (serverRoute, formData ) => {
+    axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
+    .then(({data}) => {
+      console.log(data);
+    })
+    .catch(({response}) => { toast.error(response.data.error)
+  })
+}
+  
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(authForm.current);
+
+    let serverRoute = type === "sign-in" ? "/signin" : "/signup";
+
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
 
     // formData
-    const form = new FormData(authForm.current);
+    let form = new FormData(formElement);
+    // authForm.current is the form element and we are passing it to the FormData constructor to get the form data which is stored in the form variable
     let formData = {};
-
-    for(let [key, value] of form.entries())
-    {
+    for (let [key, value] of form.entries()) {
       formData[key] = value;
     }
 
     console.log(formData);
-  }
+
+    // form validation
+    // Destructuring data from the formData
+    let { fullname, email, password } = formData;
+    if (fullname) {
+      if (fullname.length < 3) {
+        return toast.error("Fullname must be at least 3 letters long");
+      }
+    }
+    if (!email.length) {
+      return toast.error("Enter Email");
+    }
+    if (!emailRegex.test(email)) {
+      return toast.error("Invalid Email");
+    }
+    if (!passwordRegex.test(password)) {
+      return toast.error("Password must be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letter");
+    }
+
+    userAuthThroughServer(serverRoute, formData);
+  };
   return (
     <div>
       <AnimationWrapper keyValue={type}>
         <section className="h-cover flex items-center justify-center ">
-          <form ref={authForm} className="w-[80%] max-w-[400px]" onSubmit={handleSubmit}>
+          <Toaster/>
+          <form
+            id = "formElement"
+            className="w-[80%] max-w-[400px]"
+            onSubmit={handleSubmit}
+          >
             <h1 className="text-4xl font-bold font-gelasio capitalize text-center mb-24">
               {type === "sign-in" ? "Welcome Back" : "Join Us Today"}
             </h1>
@@ -58,8 +99,7 @@ const UserAuthForm = ({ type }) => {
               icon="fi-rr-key"
             />
 
-            <button className="btn-dark center mt-14" type="submit"
-            >
+            <button className="btn-dark center mt-14" type="submit">
               {type.replace("-", " ")}
             </button>
 
